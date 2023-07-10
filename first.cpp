d44645936910d33e75356095c03700e4e4d43356
joyal1414 <138481488+joyal1414@users.noreply.github.com>
2023-07-10 12:20:33 +0530

    this was my first cpp project

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<" HELLO WORLD \n wow";
+    cout<<"\nhi"<<endl;
+    cout<<"end"<<endl;
+    cout<<"\t\t\thow\t are\t \"u\"????";
+   return 0; 
+}
