a9d0053de77c64c2b7b429a6cdbf2b78afb5cd10
JOYAL RAJESH <138481488+joyal1414@users.noreply.github.com>
2023-07-18 10:45:35 +0530

    Add files via upload
    commant

---
+++ commands.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name ;
+    cout<< "enter keybord model name: "<<name;
+    cin>>name;
+
+    cout<< "entered keybord model is :";
+    return 0;
+}
