8cf72b380b82cdd44df178185ee68fc7aec65a63
JOYAL RAJESH <138481488+joyal1414@users.noreply.github.com>
2023-07-19 13:19:05 +0530

    Add files via upload
    loop enter natural number limit 19.07.2023

---
+++ loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+int i,limit;
+cout<< "enter a limit";
+cin>>limit;
+i=0;
+while (i<limit)
+{ 
+    cout<<i+1<<"\n";
+    
+    i=i+1;
+}
+
+
+
+
+
+    return 0;
+}
+/*create a program for printing N natural numbers */
